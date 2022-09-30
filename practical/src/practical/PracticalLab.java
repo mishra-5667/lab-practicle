f16461f050b5bdc31b712dd37693721505163e58
USER-HP <USER-HP@SACHIN-HP>
2022-09-30 15:56:29 +0530

    first commit

---
+++ practical/src/practical/PracticalLab.java
@@ -0,0 +1,28 @@
+package practical;
+import java.util.Scanner;
+public class PracticalLab {
+
+	 public static void main(String[] args)
+	   {
+	 //insert number
+	      int numberOne, numberTwo, largest;
+	      //create scanner objet
+	      Scanner scan = new Scanner(System.in);
+	      //print enter first number
+	      System.out.print("Enter the First Number: ");
+	      numberOne = scan.nextInt();
+	      System.out.print("Enter the Second Number: ");
+	      numberTwo = scan.nextInt();
+	      //create if condition
+	      if(numberOne>numberTwo)
+	         largest = numberOne;
+	      //create else condition
+	      else
+	         largest = numberTwo;
+	      //print largest number
+	      System.out.println("\nLargest = " +largest);
+	   }
+}
+
+
+
